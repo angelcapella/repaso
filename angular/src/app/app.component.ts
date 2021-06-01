9a4a72727b528e009f8c4299fe1d868baf9a8d14
DESKTOP-G7I7M5O\kape9 <angelcapellaruiz@gmail.com>
2021-06-01 13:28:36 +0200

    3

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ɵɵtrustConstantResourceUrl } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular';
+  userInput='soy un texto';
+
+  handleUserInput = (event : any) => {
+    this.userInput = event.currentTarget.value;
+  }
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component.component';
+import { ButtonComponent } from './button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyComponentComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
